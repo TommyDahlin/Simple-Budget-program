b2d0620038dfe2bc5eeb28f82f005d5fbe39906e
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2023-10-03 07:08:23 +0200

    first commit

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class User {
+    public String name;
+    public long id;
+
+    public User(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
